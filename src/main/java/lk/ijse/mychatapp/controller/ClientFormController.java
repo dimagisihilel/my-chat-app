f0998d9d44c39e444183b1310c2c67a07437b2fb
dimagisihilel <gunarathneedirimuni@gmail.com>
2024-01-23 13:47:42 +0530

    Initial commit

---
+++ src/main/java/lk/ijse/mychatapp/controller/ClientFormController.java
@@ -0,0 +1,27 @@
+package lk.ijse.mychatapp.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
+public class ClientFormController {
+
+    public AnchorPane pane;
+    public ScrollPane scrollPane;
+    public VBox vBox;
+
+    public Button btnCamera;
+    public Button btnEmoji;
+    public Button btnSend;
+
+    public void btnCameraOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnEmojiOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSendOnAction(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/lk/ijse/mychatapp/controller/LoginFormController.java
@@ -0,0 +1,15 @@
+package lk.ijse.mychatapp.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
+public class LoginFormController {
+
+    public AnchorPane root;
+    public Button blnLogin;
+
+    public void blnLoginOnAction(ActionEvent actionEvent) {
+
+    }
+}
